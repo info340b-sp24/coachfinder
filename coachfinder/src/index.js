70781bfba13ba1a76a27911d46e2ae623e219ef6
MagicalMT <xiong20030801@gmail.com>
2024-05-12 17:01:53 -0700

    1st draft for register and login page in react

--- coachfinder/src/index.js
+++ coachfinder/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/style.css'
+import LoginComponent from './login';
+import RegisterComponent from './register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    
+    <RegisterComponent />
   </React.StrictMode>
 );
 
---
+++ coachfinder/src/register.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import './css/style.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const RegisterComponent = () => {
+    return (
+    <html>
+    <head>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" />
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Bungee+Spice" rel="stylesheet" />
+    </head>
+
+    <body className="register-body">
+        <main>
+            <section className='all-nav'>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-orange nav-border">
+            <div className="container-fluid">
+            <a className="navbar-brand text-Kaushan mx-3" href="home.html">Coach Finder</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                <li className="nav-item mx-2 nav-item-equalspace">
+                    <a className="nav-link text-white nav-col-border" href="searchpage.html">Search</a>
+                </li>
+                <li className="nav-item mx-2 nav-item-equalspace">
+                    <a className="nav-link text-white nav-col-border" href="traineeprofile.html">Profile</a>
+                </li>
+                <li className="nav-item mx-2 nav-item-equalspace">
+                    <a className="nav-link text-white nav-col-border" href="appointmentscreen.html">Appointments</a>
+                </li>
+                <li className="nav-item mx-2 nav-item-equalspace">
+                    <a className="nav-link text-white nav-col-border" href="message.html">Messages</a>
+                </li>
+                <li className="nav-item mx-2 nav-item-equalspace">
+                    <a className="nav-link text-white nav-col-border" href="login.html">Login</a>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+        </section>
+
+        <section className="register">
+        <div className="container">
+            <div className="container-fluid pb-1 mt-5 mb-5 register-div">
+            <h1 className="fw-bold">Register</h1>
+            <div className="mb-3">
+                <label htmlFor="Useremail" className="form-label lead">Email:</label>
+                <input className="form-control" type="email" id="Useremail" name="Useremail" placeholder="xx@xx.com" required />
+                <div className="invalid-feedback">Please enter a valid email</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="Userpassword" className="form-label lead">Password:</label>
+                <input className="form-control" type="password" id="Userpassword-1" name="Userpassword-1" required minLength={8}/>
+                <div className="invalid-feedback">Password must be at least 8 characters</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="Userpassword" className="form-label lead">Password:</label>
+                <input className="form-control" type="password" id="Userpassword-2" name="Userpassword-2" required minLength={8}/>
+                <div className="invalid-feedback">Password must be at least 8 characters</div>
+            </div>
+            <div className="text-center mt-5">
+                <button className="w-50 btn btn-lg btn-primary btn-orange" type="submit">Confirm</button>
+            </div>
+            <div className="mt-3 mb-0 text-center">
+                <p>Already have an account?  <a href="register.js" className="text-start text-lightorange">Back to Login</a></p>
+            </div>
+            </div>
+        </div>
+      </section>
+
+        <footer className="register-footer">
+        <div className="container text-white">
+          <p className="text-center mt-0 mb-0">This website was created by ...</p>
+          <address className="text-center mt-0 mb-0">
+            Contact us at <a href="mailto:me@here.com">me@here.com</a>, or at <a href="tel:555-123-4567">(555) 123-4567</a>.
+          </address>
+          <p className="text-center mt-0 mb-0">&copy; 2024 The Author.</p>
+        </div>
+      </footer>
+        
+        </main>
+        </body>
+    </html>
+    );
+}
+
+    export default RegisterComponent;
