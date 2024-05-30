53dc5dfe3dac4be86987a801eb0983fc85abec2a
MagicalMT <xiong20030801@gmail.com>
2024-05-30 16:38:18 -0700

    if user do not login , profile page will redirected to login page

--- src/components/traineeProfile.js
+++ src/components/traineeProfile.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState }from 'react';
 import '../css/style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { NavBar } from './navbar';
 import { FooterComponent } from './footer'
 import { TraineeProfileInfo } from './TraineeProfileInfo'
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase/firebase';
 
 export default function TraineeProfile() {
+    const navigate = useNavigate();
+    const [userChecked, setUserChecked] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            if (!user) {
+                alert('You are not logged in. Please Login first');
+                navigate('/login');
+            }else {
+              setUserChecked(true);
+          }
+        });
+
+        return () => unsubscribe();
+    }, [navigate]);
+
   return (
     <div className="blur-effect">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" />
